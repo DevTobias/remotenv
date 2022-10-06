a33154473bb615254f0b308cdc0954bb28bfb449
Tobias Kärst <tobias.kaerst@qbeyond.de>
2022-10-06 04:24:34 +0200

    chore: initial commit

---
+++ app/src/components/Counter.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'preact/hooks';
+
+function Counter() {
+	const [count, setCount] = useState(0);
+	const add = () => setCount((i) => i + 1);
+	const subtract = () => setCount((i) => i - 1);
+
+	return (
+		<div className="counter">
+			<button type="button" onClick={subtract}>
+				-
+			</button>
+			<pre>{count}</pre>
+			<button type="button" onClick={add}>
+				+
+			</button>
+		</div>
+	);
+}
+
+export default Counter;
