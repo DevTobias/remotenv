d348783d6a2ee9174ac678ce1e433f56f0e90201
Tobias Kärst <tobias.kaerst@qbeyond.de>
2022-10-07 04:20:34 +0200

    feat: api status endpoint

--- backend/src/modules/_health/health.controller.ts
+++ backend/src/modules/_health/health.controller.ts
@@ -1,9 +1,33 @@
 import { Controller, Get } from '@nestjs/common';
+import { DiskHealthIndicator, HealthCheck, HealthCheckService, MemoryHealthIndicator } from '@nestjs/terminus';
+
+import { PrismaHealthIndicator } from 'src/modules/_health/utils/prisma-health-indicator';
 
 @Controller('health')
 export class HealthController {
-	@Get('ping')
-	ping(): string {
-		return 'pong';
-	}
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly memory: MemoryHealthIndicator,
+    private readonly disk: DiskHealthIndicator,
+    private readonly db: PrismaHealthIndicator
+  ) {}
+
+  @Get('ping')
+  ping(): string {
+    return 'pong';
+  }
+
+  @Get('status')
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.db.isHealthy('database'),
+      () => this.memory.checkHeap('backend_memory', 150 * 1024 * 1024),
+      () =>
+        this.disk.checkStorage('backend_storage', {
+          path: '/',
+          thresholdPercent: 0.5,
+        }),
+    ]);
+  }
 }
--- backend/src/modules/_health/health.module.ts
+++ backend/src/modules/_health/health.module.ts
@@ -1,5 +1,14 @@
 import { Module } from '@nestjs/common';
-import { HealthController } from 'src/modules/_health/health.controller';
+import { TerminusModule } from '@nestjs/terminus';
+import { HttpModule } from '@nestjs/axios';
 
-@Module({ controllers: [HealthController] })
+import { HealthController } from 'src/modules/_health/health.controller';
+import { PrismaModule } from 'src/modules/_setup/prisma/prisma.module';
+import { PrismaHealthIndicator } from 'src/modules/_health/utils/prisma-health-indicator';
+
+@Module({
+  imports: [TerminusModule, HttpModule, PrismaModule],
+  providers: [PrismaHealthIndicator],
+  controllers: [HealthController],
+})
 export class HealthModule {}
