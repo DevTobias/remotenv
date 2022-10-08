678b9ff6aae9caa9d3b1858c0ab3fb0940f0d852
Tobias Kärst <tobias.kaerst@qbeyond.de>
2022-10-08 18:23:15 +0200

    deploy: dockerized application

--- backend/src/modules/_setup/config/utils/config-validation.ts
+++ backend/src/modules/_setup/config/utils/config-validation.ts
@@ -1,15 +1,15 @@
 import { z } from 'zod';
 
 export const envValidationSchema = z.object({
-  NODE_ENV: z.enum(['development', 'production', 'test']),
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
 
-  DATABASE_FILE: z.string(),
+  DATABASE_FILE: z.string().default('file:./remotenv.db'),
 
-  THROTTLE_TTL: z.preprocess(parseInt, z.number()),
-  THROTTLE_LIMIT: z.preprocess(parseInt, z.number()),
+  THROTTLE_TTL: z.preprocess(parseInt, z.number()).default(60),
+  THROTTLE_LIMIT: z.preprocess(parseInt, z.number()).default(10),
 
-  API_PORT: z.preprocess(parseInt, z.number()),
-  API_HOST: z.string(),
-  API_PREFIX: z.string(),
-  STATIC_PATH: z.string(),
+  API_PORT: z.preprocess(parseInt, z.number()).default(3420),
+  API_HOST: z.string().default('0.0.0.0'),
+  API_PREFIX: z.string().default('api/v1'),
+  STATIC_PATH: z.string().default('../app'),
 });
